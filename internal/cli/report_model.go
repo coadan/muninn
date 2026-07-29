5ce5535a30a871b946595690a5110f972d72bf04
Vegard <vegard@vs-macbook.lan>
2026-07-29 16:03:37 +0200

    Version fallback-aware discovery findings

--- internal/cli/report_model.go
+++ internal/cli/report_model.go
@@ -2,7 +2,7 @@ package cli
 
 import "time"
 
-const codexSessionInsightsSchemaVersion = 72
+const codexSessionInsightsSchemaVersion = 73
 
 type normalizedTokenUsage struct {
 	InputTokens         int64 `json:"inputTokens"`
